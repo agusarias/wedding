baf33fb444f1a15358309cfc29ae4086c3bbf4f6
Agustin Arias <agustin@tiendanube.com>
2019-02-13 12:11:18 -0300

    Casi, falta definir colores, y details

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -4,12 +4,10 @@ function find(id) {
   return document.getElementById(id);
 }
 
-function find(id) {
-  return document.getElementById(id);
-}
-
 // Init
 document.addEventListener("DOMContentLoaded", function() {
+  showHeaderImage();
+
   var header = find("header");
   header.onclick = function() {
     window.scrollBy({
@@ -19,17 +17,13 @@ document.addEventListener("DOMContentLoaded", function() {
       pasive: true
     });
   };
-
-  var modals = ["civil", "fiesta"];
-  modals.forEach(function(key) {
-    find("trigger-" + key).onclick = function() {
-      var modal = find("modal-" + key);
-      modal.classList.add("active");
-      modal.querySelectorAll(".js-close").forEach(function(closeButton) {
-        closeButton.onclick = function() {
-          modal.classList.remove("active");
-        };
-      });
-    };
-  });
 });
+
+function showHeaderImage() {
+  if (window.innerWidth >= 797) {
+    var image = find("main-image-desktop");
+  } else {
+    var image = find("main-image-mobile");
+  }
+  image.src = image.dataset.src;
+}
